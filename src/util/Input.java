a02b65b5e9dbd2989fdcb78504ed29018abd3dac
WilliamBaldridge <baldridge.william09@gmail.com>
2021-03-11 09:43:54 -0600

    OOP exercises

---
+++ src/util/Input.java
@@ -0,0 +1,54 @@
+package util;
+
+import java.util.Scanner;
+
+public class Input {
+
+    Scanner Input = new Scanner(System.in);
+
+
+    public String getString() {
+
+        System.out.println("Enter a string");
+        String userInput = Input.nextLine();
+        return userInput;
+    }
+
+    public boolean yesNo() {
+
+
+
+    }
+
+
+    public int getInt(int min, int max) {
+
+
+
+    }
+
+    public int getInt() {
+
+
+
+    }
+
+    public double getDouble(double min, double max) {
+
+
+
+    }
+
+    public double getDouble() {
+
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+}

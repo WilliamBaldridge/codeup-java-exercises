d00a34d8579c369406174620b8ab6894a775fe3b
WilliamBaldridge <baldridge.william09@gmail.com>
2021-03-11 20:16:40 -0600

    Arrays exercises

---
+++ src/ArraysExercises.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import util.Input;
+
+public class ArraysExercises {
+
+
+    Person[] person1;
+
+    person1 = new Person[3];
+
+
+
+
+
+    public static void main(String[] args) {
+
+
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(numbers));
+
+
+    }
+
+
+}

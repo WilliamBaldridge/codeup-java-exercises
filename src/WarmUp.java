2b3da4a712793df072cc9c42b204d5d9012ff5dc
WilliamBaldridge <baldridge.william09@gmail.com>
2021-04-12 09:24:56 -0500

    Java warmup

--- src/WarmUp.java
+++ src/WarmUp.java
@@ -21,23 +21,54 @@ public class WarmUp {
 //
 //    }
 
-    public static boolean secondListIsOneMore(List<Integer> lOne, List<Integer> lTwo) {
-        for (int i = 0; i < lOne.size(); i += 1) {
-            if (lOne.get(i) + 1 != lTwo.get(i)) {
-                return false;
-            }
-        }
-        return true;
+//    public static boolean secondListIsOneMore(List<Integer> lOne, List<Integer> lTwo) {
+//        for (int i = 0; i < lOne.size(); i += 1) {
+//            if (lOne.get(i) + 1 != lTwo.get(i)) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+//
+//    public static void main(String[] args) {
+//        List<Integer> lOne = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+//        List<Integer> lTwo = new ArrayList<>(Arrays.asList(2, 3, 4, 5));
+//        List<Integer> lThree = new ArrayList<>(Arrays.asList(3, 4, 5, 6));
+//        System.out.println(secondListIsOneMore(lOne, lTwo)); // should return true
+//        System.out.println(secondListIsOneMore(lOne, lThree)); // should return false
+//        System.out.println(secondListIsOneMore(lTwo, lThree)); // should return true
+//    }
+
+
+
+   /* TODO: In Java exercises, create a class, JavaIIIWarmUp, and include the following. You will also need to create a main method to test it. Create a static method, sameFirstAndLast, that takes in a string and returns true if the first and last character match (not case sensitive).
+Examples:
+  Input  - “Bob”
+  Output - true
+  Input  - “Steve”
+  Output - false
+  Input  - “refrigerator”
+  Output - true
+  Input  - “q2kdj3vq”
+  Output - true
+    */
+
+    static boolean sameFirstAndLast (String inputString) {
+
+        char firstChar = Character.toLowerCase(inputString.charAt(0));
+        char lastChar = Character.toLowerCase(inputString.charAt(inputString.length() - 1));
+
+        return firstChar == lastChar;
+
     }
 
     public static void main(String[] args) {
-        List<Integer> lOne = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-        List<Integer> lTwo = new ArrayList<>(Arrays.asList(2, 3, 4, 5));
-        List<Integer> lThree = new ArrayList<>(Arrays.asList(3, 4, 5, 6));
-        System.out.println(secondListIsOneMore(lOne, lTwo)); // should return true
-        System.out.println(secondListIsOneMore(lOne, lThree)); // should return false
-        System.out.println(secondListIsOneMore(lTwo, lThree)); // should return true
+        System.out.println(sameFirstAndLast("Bob"));
+        System.out.println(sameFirstAndLast("steve"));
+        System.out.println(sameFirstAndLast("refrigerator"));
+        System.out.println(sameFirstAndLast("q2kdj3vq"));
     }
+
 
 }
 
